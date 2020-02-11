41f7326e62eadd99c43613e5d4b6e65d9271016d
melvingelbard <melvingelbard@users.noreply.github.com>
2020-02-11 10:03:01 +0000

    Cleaned up

--- qupath-gui-fx/src/main/java/qupath/lib/gui/panels/PathClassPane.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/panels/PathClassPane.java
@@ -32,7 +32,6 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SelectionMode;
